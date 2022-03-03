025fb8e4c74eb91a7f15ab74a73387be1a30e248
DoganK01 <98788987+DoganK01@users.noreply.github.com>
2022-03-03 21:57:41 +0300

    Update train_and_detect.py

--- train_and_detect.py
+++ train_and_detect.py
@@ -6,7 +6,10 @@ from keras.layers import Dense, Dropout, Activation, Flatten, BatchNormalization
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers.advanced_activations import ELU
 import os
-
+import cv2
+import numpy as np
+from time import sleep
+from keras.preprocessing.image import img_to_array
 
 num_classes = 7
 img_rows, img_cols = 48, 48
@@ -185,3 +188,53 @@ tick_marks = np.arrange(len(classes))
 
 _=plt.xticks(tick_marks, classes, rotation=90)
 _=plt.yticks(tick_marks, classes)
+
+
+
+
+#REAL_TIME_DETECT
+
+face_classifier = cv2.CascadeClassifier('./Haarcascades/haarcascade_frontalface_default.xml')
+
+def face_detector(img):
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray, 1.3, 5)
+    if face is ():
+        return (0,0,0,0), np.zeros((48,48), np.uint8), img
+    for(x,y,w,h) in faces:
+        x = x - 50
+        w = w + 50
+        y = y - 50
+        h = h + 50
+        cv2.rectangle(img, (x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+    try:
+        roi_gray = cv2.resize(roi_gray, (48,48), interpolation = cv2.INTER_AREA)
+    except:
+        return (x,w,y,h), np.zeros((48,48), np.uint8), img
+    return (x,w,y,h), roi_gray, img
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    rect, face, image = face_detector(frame)
+    if np.sum([face]) !=0.0:
+        roi = face.astype("float")/255.0
+        roi = img_to_array(roi)
+        roi = np.expand_dims(roi, axis=0)
+        
+        
+        preds = classifier.predict(roi)[0]
+        label = class_labels[preds.argmax()]
+        label_position = (rect[0] + int((rect[1]/2)), rect[2] + 25)
+        cv2.putText(image, label, label_position, cv2.FONT_HERSHEY_SIMPLEX,2, (0,255,0),3)
+    else:
+        cv2.putText(image, "No Face Found", (20,60), cv2.FONT_HERSHEY_SIMPLEX,2, (0,255,0),3)
+        
+    cv2.imshow('ALL', image)
+    if cv2.waitKey(1) == 13: #Enter Key
+        break
+cap.release()
+cv2.destroyAllWindows()
+        
+        
